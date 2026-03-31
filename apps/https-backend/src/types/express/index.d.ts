e4c7f81dc0fc20cd1c44e885029ac60d7d8e9943
hariom.kumar <hariom.kumar@vwo.com>
2026-03-31 21:43:58 +0530

    feat(): initial turbo

--- apps/https-backend/src/types/express/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { JwtPayload } from "jsonwebtoken";
-
-declare global {
-  namespace Express {
-    interface Request {
-      userId?: number; // or number, depending on your user ID type
-    }
-  }
-}
